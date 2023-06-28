385ade83b5888f29257a60ad11b8003128659684
jay hsu <jayh0324@gmail.com>
2023-06-28 16:57:32 +0800

    上下對折效果
    Signed-off-by: jay hsu <jayh0324@gmail.com>

--- js/pinPannel.js
+++ js/pinPannel.js
@@ -13,6 +13,23 @@ ScrollTrigger.scrollerProxy(".scroller", {
 });
 bodyScrollBar.addListener(ScrollTrigger.update);
 
+
+//sectionWraper
+gsap.to("#sectionWraper", {
+  'top': 0, 
+  ease: "none",
+  stagger: 0.5,
+  scrollTrigger: {
+    trigger: "#FirstContent",
+    start: "top top",
+    end: "+=100%",
+    scrub: true,
+    pin: true,
+    pinSpacing:false,
+    scroller: ".scroller",
+  }
+});
+
 //caseContent
 gsap.set(".panel", { zIndex: (i, target, targets) => targets.length - i });
 var images = gsap.utils.toArray(".panel:not(:last-child)");
@@ -43,33 +60,52 @@ ScrollTrigger.create({
   start: () => "top top",
   end: () => "+=" + (images.length + 1) * window.innerHeight,
   invalidateOnRefresh: true,
+  onLeave: () => {
+    gsap.to("#caseContent", {
+      ease: "none",
+      stagger: 0.5,
+      scrollTrigger: {
+        trigger: ".panel:last-child",
+        start: "bottom bottom",
+        end: "+=100%",
+        scrub: true,
+        pin: true,
+        pinSpacing:false,
+        scroller: ".scroller",
+        // markers: true,
+      }
+    });
+  },
 });
+
+//sectionWraper
+
 
 //layer移動 (往上蓋)
-let panels = gsap.utils.toArray(".trans_panel");
-let tops = panels.map(panel => ScrollTrigger.create({trigger: panel, start: "top top"}));
+// let panels = gsap.utils.toArray(".trans_panel");
+// let tops = panels.map(panel => ScrollTrigger.create({trigger: panel, start: "top top"}));
 
-panels.forEach((panel, i) => {
-  ScrollTrigger.create({
-    trigger: panel,
-    start: () => panel.offsetHeight < window.innerHeight ? "top top" : "bottom bottom",
-    pin: true, 
-    pinSpacing: false,
-    scroller: ".scroller",
-    // markers: true
-  });
-});
+// panels.forEach((panel, i) => {
+//   ScrollTrigger.create({
+//     trigger: panel,
+//     start: () => panel.offsetHeight < window.innerHeight ? "top top" : "bottom bottom",
+//     pin: true, 
+//     pinSpacing: false,
+//     scroller: ".scroller",
+//     // markers: true
+//   });
+// });
 
-ScrollTrigger.create({
-  snap: {
-    snapTo: (progress, self) => {
-      let panelStarts = tops.map(st => st.start),
-          snapScroll = gsap.utils.snap(panelStarts, self.scroll());
-      return gsap.utils.normalize(0, ScrollTrigger.maxScroll(window), snapScroll);
-    },
-    duration: 0.5
-  }
-});
+// ScrollTrigger.create({
+//   snap: {
+//     snapTo: (progress, self) => {
+//       let panelStarts = tops.map(st => st.start),
+//           snapScroll = gsap.utils.snap(panelStarts, self.scroll());
+//       return gsap.utils.normalize(0, ScrollTrigger.maxScroll(window), snapScroll);
+//     },
+//     duration: 0.5
+//   }
+// });
 
 //layer移動 (往上翻)
 // gsap.to(".trans_panel:not(:last-child)", {
@@ -85,6 +121,5 @@ ScrollTrigger.create({
 //     scroller: ".scroller",
 //   }
 // });
-
 
 // gsap.set(".trans_panel", {zIndex: (i, target, targets) => targets.length - i});
