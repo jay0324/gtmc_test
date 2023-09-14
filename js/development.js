dcd4cbc37ff2aba27e36504ed3c23e405de14055
jay hsu <jayh0324@gmail.com>
2023-09-14 15:40:18 +0800

    修改svg動畫，繞圈圈的那個
    Signed-off-by: jay hsu <jayh0324@gmail.com>

--- js/development.js
+++ js/development.js
@@ -2,6 +2,12 @@ $(function(){
     if ($('#development').length > 0) {
         var $target = $('#development');
         var trigger_development = false;
+        var circle_mask = document.getElementById('circle_mask_main');
+        var circle_mask_length = circle_mask.getTotalLength();
+        $('#circle_mask_main').css({
+            'stroke-dasharray': circle_mask_length,
+            'stroke-dashoffset': circle_mask_length,
+        });
         $("#wrapwrap").on('scroll', () => {
             var pos = $("#development_section").offset().top - $(window).height();
             if (pos < 0 && !trigger_development) {
@@ -12,29 +18,59 @@ $(function(){
                 .from($("#center", $target), {
                     opacity: 0
                 })
-                .from($("#top_x5F_line", $target), {
+                .from($("#center_x5F_1", $target), {
                     opacity: 0
+                })
+                .from($("#center_x5F_2", $target), {
+                    opacity: 0,
+                    delay: 0.5
+                })
+                .from($("#center_x5F_3", $target), {
+                    opacity: 0,
+                    delay: 0.5
+                })
+                .from($("#top_x5F_line_x5F_body", $target), {
+                    opacity: 0,
+                    delay: 0.5,
+                    transform: 'scaleY(0)',
+                    'transform-origin': 'center'
+                })
+                .from($("#top_x5F_dot", $target), {
+                    opacity: 0,
                 })
                 .from($("#word_x5F_top", $target), {
                     opacity: 0
                 })
-                .from($("#left_x5F_line", $target), {
+                .from($("#left_x5F_line_x5F_body", $target), {
+                    opacity: 0,
+                    transform: 'scaleX(0)',
+                    'transform-origin': 'center'
+                })
+                .from($("#left_x5F_dot", $target), {
                     opacity: 0
                 })
                 .from($("#word_x5F_left", $target), {
-                    opacity: 0
+                    opacity: 0,
                 })
-                .from($("#right_x5F_line_1_", $target), {
+                .from($("#right_x5F_line_x5F_body", $target), {
+                    opacity: 0,
+                    transform: 'scaleX(0)',
+                    'transform-origin': 'center'
+                })
+                .from($("#right_x5F_dot", $target), {
                     opacity: 0
                 })
                 .from($("#word_x5F_right", $target), {
                     opacity: 0
                 })
                 .from($("#bottom_x5F_line", $target), {
-                    opacity: 0
+                    opacity: 0,
+                    transform: 'scaleY(0)',
+                    'transform-origin': 'center'
                 })
-                .from($("#circle", $target), {
-                    opacity: 0
+                .to($("#circle_mask_main", $target), {
+                    'stroke-dashoffset': 0,
+                    duration: 3
                 })
                 .from($("#arraw", $target), {
                     opacity: 0
